b0b1a1b0df8c1906fd66d948f3311fe380636612
yassine94 <70703274+yassine94@users.noreply.github.com>
2024-09-12 14:57:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #commentary
   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+  
 if __name__ == "__main__":
   app.run(debug=True)
