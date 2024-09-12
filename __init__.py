50a1c224285afe02f7d775da9d16d0f7e7002c58
yassine94 <70703274+yassine94@users.noreply.github.com>
2024-09-12 15:10:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,36 +4,31 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #commentary
-  
+                                                                                                                                       
+app = Flask(__name__)   
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return "<h2>Ma page de contact</h2>"
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #Comm
 
 @app.route('/tawarano/')
 def meteo():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-  
-for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 
 @app.route("/rapport/")
 def mongraphique():
-    return render_template("graphique.html")
-
-
-
+    return render_template("graphique.html")
+  
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True)
