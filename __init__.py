5c6fcc0c075c2b917d0f40e52e8674e62307b9a7
yassine94 <70703274+yassine94@users.noreply.github.com>
2024-09-12 15:42:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route("/histogramme/")
-def mongraphique():
+def graphique():
     return render_template("histogramme.html")
 
 
