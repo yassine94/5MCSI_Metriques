50bbf845ee5381a5e3a2c210ffa0c442738fccef
yassine94 <70703274+yassine94@users.noreply.github.com>
2024-09-12 16:04:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,119 +1,38 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contactez-nous - ESGI</title>
-    <style>
-        body {
-            font-family: 'Roboto', sans-serif;
-            background-image: url('https://www.esgi.fr/assets/images/home-page/banner1.png'); /* Image ESGI */
-            background-size: cover;
-            background-position: center;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            color: white;
-            text-align: center;
-        }
-        .overlay {
-            background-color: rgba(0, 0, 0, 0.7);
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-        }
-        .contact-container {
-            background-color: rgba(255, 255, 255, 0.1);
-            border-radius: 20px;
-            padding: 40px;
-            max-width: 500px;
-            width: 100%;
-            z-index: 1;
-            position: relative;
-            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-        }
-        h2 {
-            font-size: 36px;
-            margin-bottom: 20px;
-            font-weight: bold;
-            color: #F1C40F;
-        }
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        label {
-            margin: 10px 0 5px;
-            color: #F1C40F;
-            font-weight: bold;
-        }
-        input, textarea {
-            padding: 12px;
-            border-radius: 10px;
-            border: none;
-            margin-bottom: 20px;
-            font-size: 16px;
-            width: 100%;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-            outline: none;
-        }
-        input:focus, textarea:focus {
-            border: 2px solid #F1C40F;
-        }
-        button {
-            padding: 12px 20px;
-            border-radius: 50px;
-            background-color: #F1C40F;
-            color: black;
-            border: none;
-            font-size: 18px;
-            cursor: pointer;
-            font-weight: bold;
-            transition: background-color 0.3s ease;
-        }
-        button:hover {
-            background-color: #e1b009;
-        }
-        .esgi-logo {
-            max-width: 120px;
-            margin: 20px auto;
-        }
-        .footer {
-            margin-top: 20px;
-            font-size: 12px;
-            color: #ccc;
-        }
-    </style>
-</head>
-<body>
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
+from datetime import datetime
+from urllib.request import urlopen
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__)   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("contact.html")
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #Comm
 
-<div class="overlay"></div>
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 
-<div class="contact-container">
-    <img class="esgi-logo" src="https://www.esgi.fr/assets/images/logo-esgi-blanc.png" alt="ESGI Logo">
-    <h2>Contactez-nous</h2>
-    <form>
-        <label for="first_name">Prénom</label>
-        <input type="text" id="first_name" name="first_name" placeholder="Votre prénom" required>
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 
-        <label for="last_name">Nom</label>
-        <input type="text" id="last_name" name="last_name" placeholder="Votre nom" required>
-
-        <label for="message">Message</label>
-        <textarea id="message" name="message" rows="5" placeholder="Votre message..." required></textarea>
-
-        <button type="submit">Envoyer</button>
-    </form>
-    <div class="footer">
-        © 2024 ESGI - École Supérieure de Génie Informatique
-    </div>
-</div>
-
-</body>
-</html>
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+  
+if __name__ == "__main__":
+  app.run(debug=True)
